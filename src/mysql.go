952ed5972b178271bcb13b805c1ac22c02f0d725
Mirko Krause <krause@codebase.one>
2020-01-01 05:48:07 +0100

    introduced exclusion for get and poll queries in backend

--- src/mysql.go
+++ src/mysql.go
@@ -23,14 +23,16 @@ CREATE TABLE IF NOT EXISTS Items (
 	INDEX item_index (queue, position)
 )
 `
-	createQueue      = "INSERT INTO Queues VALUES (NULL, NULL)"
-	createNamedQueue = "INSERT IGNORE INTO Queues VALUES (NULL, ?)"
-	getNamedQueue    = "SELECT * FROM Queues WHERE name=?"
-	appendItem       = "INSERT INTO Items (queue, data) VALUES (?, ?)"
-	deleteItem       = "DELETE FROM Items WHERE position=?"
-	getQueue         = "SELECT position, data FROM Items WHERE queue=? ORDER BY position"
-	getFirstItem     = getQueue + " ASC LIMIT 1"
-	getRandomItem    = `
+	createQueue            = "INSERT INTO Queues VALUES (NULL, NULL)"
+	createNamedQueue       = "INSERT IGNORE INTO Queues VALUES (NULL, ?)"
+	getNamedQueue          = "SELECT * FROM Queues WHERE name=?"
+	appendItem             = "INSERT INTO Items (queue, data) VALUES (?, ?)"
+	deleteItem             = "DELETE FROM Items WHERE position=?"
+	getQueue               = "SELECT position, data FROM Items WHERE queue=? ORDER BY position"
+	getQueueButExclude     = "SELECT position, data FROM Items WHERE queue=? AND position NOT IN (SELECT data FROM Items WHERE queue=?) ORDER BY position"
+	getFirstItem           = getQueue + " ASC LIMIT 1"
+	getFirstItemButExclude = getQueueButExclude + " ASC LIMIT 1"
+	getRandomItem          = `
 SELECT position, data 
 FROM Items AS r1 
 JOIN (
@@ -46,21 +48,41 @@ WHERE r1.queue=? AND r1.position >= r2.id
 ORDER BY r1.position ASC
 LIMIT 1
 `
-	getLastItem  = getQueue + " DESC LIMIT 1"
-	getQueueSize = "SELECT COUNT(*) FROM Items WHERE queue=?"
+	getRandomItemButExclude = `
+SELECT position, data 
+FROM Items AS r1 
+JOIN (
+	SELECT CEIL(RAND() * (
+		SELECT MAX(position) - MIN(position)
+		FROM Items
+	) + (
+		SELECT MIN(position) 
+		FROM Items
+	)) AS id
+) AS r2
+WHERE r1.queue=? AND r1.position >= r2.id AND r1.position NOT IN (SELECT data FROM Items WHERE queue=?)
+ORDER BY r1.position ASC
+LIMIT 1
+`
+	getLastItem           = getQueue + " DESC LIMIT 1"
+	getLastItemButExclude = getQueueButExclude + " DESC LIMIT 1"
+	getQueueSize          = "SELECT COUNT(*) FROM Items WHERE queue=?"
 )
 
 type MySQLStorage struct {
-	db               *sql.DB
-	appendItem       *sql.Stmt
-	deleteItem       *sql.Stmt
-	getQueue         *sql.Stmt
-	getFirstItem     *sql.Stmt
-	getRandomItem    *sql.Stmt
-	getLastItem      *sql.Stmt
-	getQueueSize     *sql.Stmt
-	createNamedQueue *sql.Stmt
-	getNamedQueue    *sql.Stmt
+	db                      *sql.DB
+	appendItem              *sql.Stmt
+	deleteItem              *sql.Stmt
+	getQueue                *sql.Stmt
+	getFirstItem            *sql.Stmt
+	getFirstItemButExclude  *sql.Stmt
+	getRandomItem           *sql.Stmt
+	getRandomItemButExclude *sql.Stmt
+	getLastItem             *sql.Stmt
+	getLastItemButExclude   *sql.Stmt
+	getQueueSize            *sql.Stmt
+	createNamedQueue        *sql.Stmt
+	getNamedQueue           *sql.Stmt
 }
 
 func NewMySQL(config Configuration) (*MySQLStorage, error) {
@@ -106,17 +128,35 @@ func NewMySQL(config Configuration) (*MySQLStorage, error) {
 	}
 	storage.getFirstItem = getFirstItemStatement
 
+	getFirstItemButExcludeStatement, err := db.Prepare(getFirstItemButExclude)
+	if err != nil {
+		return nil, err
+	}
+	storage.getFirstItemButExclude = getFirstItemButExcludeStatement
+
 	getRandomItemStatement, err := db.Prepare(getRandomItem)
 	if err != nil {
 		return nil, err
 	}
 	storage.getRandomItem = getRandomItemStatement
 
+	getRandomItemButExcludeStatement, err := db.Prepare(getRandomItemButExclude)
+	if err != nil {
+		return nil, err
+	}
+	storage.getRandomItemButExclude = getRandomItemButExcludeStatement
+
 	getLastItemStatement, err := db.Prepare(getLastItem)
 	if err != nil {
 		return nil, err
 	}
 	storage.getLastItem = getLastItemStatement
+
+	getLastItemButExcludeStatement, err := db.Prepare(getLastItemButExclude)
+	if err != nil {
+		return nil, err
+	}
+	storage.getLastItemButExclude = getLastItemButExcludeStatement
 
 	getQueueSizeStatement, err := db.Prepare(getQueueSize)
 	if err != nil {
@@ -160,12 +200,27 @@ func (storage *MySQLStorage) Close() error {
 		return err
 	}
 
+	err = storage.getFirstItemButExclude.Close()
+	if err != nil {
+		return err
+	}
+
 	err = storage.getRandomItem.Close()
 	if err != nil {
 		return err
 	}
 
+	err = storage.getRandomItemButExclude.Close()
+	if err != nil {
+		return err
+	}
+
 	err = storage.getLastItem.Close()
+	if err != nil {
+		return err
+	}
+
+	err = storage.getLastItemButExclude.Close()
 	if err != nil {
 		return err
 	}
@@ -361,8 +416,50 @@ func getElement(queue int, stmt *sql.Stmt) (*ListItem, error) {
 	return &listItem, nil
 }
 
+func getElementExcluding(elementQueue int, excludeQueue int, stmt *sql.Stmt) (*ListItem, error) {
+	results, err := stmt.Query(elementQueue, excludeQueue)
+	if err != nil {
+		return nil, err
+	}
+
+	results.Next()
+	var position int
+	var dataString string
+	err = results.Scan(&position, &dataString)
+	if err != nil {
+		return nil, err
+	}
+
+	var listItem = ListItem{
+		Position: position,
+		Data:     Data{dataString},
+	}
+
+	if results.Next() {
+		// this condition is expected to be false
+		log.Println("Got unexpected second result for first Item")
+		// do not investigate any further
+	}
+
+	err = results.Close()
+	if err != nil {
+		return &listItem, err
+	}
+
+	return &listItem, nil
+}
+
 func (storage *MySQLStorage) pollElement(queue int, stmt *sql.Stmt) (*ListItem, error) {
 	item, err := getElement(queue, stmt)
+	if err != nil {
+		return item, err
+	}
+	err = storage.Delete(item.Position)
+	return item, err
+}
+
+func (storage *MySQLStorage) pollElementExcluding(elementQueue int, excludeQueue int, stmt *sql.Stmt) (*ListItem, error) {
+	item, err := getElementExcluding(elementQueue, excludeQueue, stmt)
 	if err != nil {
 		return item, err
 	}
@@ -374,22 +471,46 @@ func (storage *MySQLStorage) GetFirstElement(queue int) (*ListItem, error) {
 	return getElement(queue, storage.getFirstItem)
 }
 
+func (storage *MySQLStorage) GetFirstElementExcluding(elementQueue int, excludeQueue int) (*ListItem, error) {
+	return getElementExcluding(elementQueue, excludeQueue, storage.getFirstItem)
+}
+
 func (storage *MySQLStorage) PollFirstElement(queue int) (*ListItem, error) {
 	return storage.pollElement(queue, storage.getFirstItem)
+}
+
+func (storage *MySQLStorage) PollFirstElementExcluding(elementQueue int, excludeQueue int) (*ListItem, error) {
+	return storage.pollElementExcluding(elementQueue, excludeQueue, storage.getFirstItem)
 }
 
 func (storage *MySQLStorage) GetRandomElement(queue int) (*ListItem, error) {
 	return getElement(queue, storage.getRandomItem)
 }
 
+func (storage *MySQLStorage) GetRandomElementExcluding(elementQueue int, excludeQueue int) (*ListItem, error) {
+	return getElementExcluding(elementQueue, excludeQueue, storage.getRandomItemButExclude)
+}
+
 func (storage *MySQLStorage) PollRandomElement(queue int) (*ListItem, error) {
 	return storage.pollElement(queue, storage.getRandomItem)
+}
+
+func (storage *MySQLStorage) PollRandomElementExcluding(elementQueue int, excludeQueue int) (*ListItem, error) {
+	return storage.pollElementExcluding(elementQueue, excludeQueue, storage.getRandomItem)
 }
 
 func (storage *MySQLStorage) GetLastElement(queue int) (*ListItem, error) {
 	return getElement(queue, storage.getLastItem)
 }
 
+func (storage *MySQLStorage) GetLastElementExcluding(elementQueue int, excludeQueue int) (*ListItem, error) {
+	return getElementExcluding(elementQueue, excludeQueue, storage.getLastItem)
+}
+
 func (storage *MySQLStorage) PollLastElement(queue int) (*ListItem, error) {
 	return storage.pollElement(queue, storage.getLastItem)
+}
+
+func (storage *MySQLStorage) PollLastElementExcluding(elementQueue int, excludeQueue int) (*ListItem, error) {
+	return storage.pollElementExcluding(elementQueue, excludeQueue, storage.getLastItem)
 }
